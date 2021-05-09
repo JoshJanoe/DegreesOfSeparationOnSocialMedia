3214f6e608249ed990ed2774e22567949b7d3d17
Josh Janoe <61761928+JoshJanoe@users.noreply.github.com>
2021-05-09 11:14:42 -0500

    Add files via upload

--- src/CapstoneProject/CapstoneTester.java
+++ src/CapstoneProject/CapstoneTester.java
@@ -14,6 +14,9 @@ public class CapstoneTester {
 
     private CapstoneDoSGraph testCG;
     private int numNodes = 7;
+    private String filename = "data/facebook_ucsd.txt";
+    //private String filename = "data/mutualFriendsTest.txt";
+    //private String filename = "data/nthDegreeConnectionsTest.txt";
 
     public CapstoneTester(){
 
@@ -23,26 +26,14 @@ public class CapstoneTester {
     public void smallTestSetup(){
         testCG = new CapstoneDoSGraph();
 
-        testCG.addVertex(0);
-        testCG.addVertex(1);
-        testCG.addVertex(2);
-        testCG.addVertex(3);
-        testCG.addVertex(4);
-        testCG.addVertex(5);
+        testCG.addVertex(0);        testCG.addVertex(1);        testCG.addVertex(2);
+        testCG.addVertex(3);        testCG.addVertex(4);        testCG.addVertex(5);
         testCG.addVertex(6);
 
-        testCG.addEdge(0, 6);
-        testCG.addEdge(1, 3);
-        testCG.addEdge(2, 1);
-        testCG.addEdge(2, 5);
-        testCG.addEdge(3, 4);
-        testCG.addEdge(3, 5);
-        testCG.addEdge(4, 3);
-        testCG.addEdge(4, 5);
-        testCG.addEdge(5, 0);
-        testCG.addEdge(6, 0);
-        testCG.addEdge(6, 2);
-        testCG.addEdge(6, 4);
+        testCG.addEdge(0, 6);        testCG.addEdge(1, 3);        testCG.addEdge(2, 1);
+        testCG.addEdge(2, 5);        testCG.addEdge(3, 4);        testCG.addEdge(3, 5);
+        testCG.addEdge(4, 3);        testCG.addEdge(4, 5);        testCG.addEdge(5, 0);
+        testCG.addEdge(6, 0);        testCG.addEdge(6, 2);        testCG.addEdge(6, 4);
     }
 
     //@Test
@@ -50,52 +41,100 @@ public class CapstoneTester {
         smallTestSetup();
         assertEquals(numNodes,getNumNodes());
         assertEquals(12,getNumEdges());
-
         System.out.println("Original Graph:");
         printGraph();
-
+        //
+        System.out.println("-------");
+        //
+        testCG = new CapstoneDoSGraph();
+        loadGraph(testCG, "data/small_test_1.txt");
+        assertEquals(numNodes,getNumNodes());
+        assertEquals(18,getNumEdges());
+        System.out.println("Original Graph:");
+        printGraph();
     }
 
-    @Test
-    public void fb1000Test (){
+    //@Test
+    public void connectionsTest (){
         testCG = new CapstoneDoSGraph();
-        loadGraph(testCG, "data/facebook_1000.txt");
+        loadGraph(testCG, filename);
 
         //Connections Test
         assertEquals(9,testCG.getConnections(0).size());
         printConnections(0);
         System.out.println("----------------");
+    }
 
-        //Mutual Friends Test
-        assertEquals(1, testCG.getMutualFriends(0,64).size());
-        printMutualFriends(0,64);
+    //@Test
+    public void mutualFriendsTest (){
+        testCG = new CapstoneDoSGraph();
+        loadGraph(testCG, filename);
+        assertEquals(4, testCG.getMutualFriends(3,8).size());
+        printMutualFriends(3,8);
         System.out.println("----------------");
+    }
 
-        //Degree of Separation Test
-        Integer testStart = 103;
-        Integer testEnd = 887;
+    //@Test
+    public void degreeOfSeparationTest (){
+        testCG = new CapstoneDoSGraph();
+        loadGraph(testCG, filename);
+        Integer testStart = 2;
+        Integer testEnd = 8;
+        Integer target = 3;
         Integer dOS = testCG.getDegreeOfSeparation(testStart,testEnd);
+        assertEquals(target, dOS);
         System.out.println("Degrees of Separation: "+dOS);
         printPath(testStart,testEnd);
-        assertEquals(4,testCG.getDegreeOfSeparation(103,887));
         System.out.println("----------------");
+    }
 
-        //nth Degree Connections Test
+    //@Test
+    public void nthDegreeConnectionsTest (){
+        testCG = new CapstoneDoSGraph();
+        loadGraph(testCG, filename);
         Integer userN = 1;
         printNthDegreeConnections(userN,0);
         printNthDegreeConnections(userN,1);
         printNthDegreeConnections(userN,2);
         printNthDegreeConnections(userN,3);
+        printNthDegreeConnections(userN,4);
+        printNthDegreeConnections(userN,5);
+        printNthDegreeConnections(userN,6);
+        printNthDegreeConnections(userN,7);
         System.out.println("----------------");
+    }
 
-        //2nd degree mutual connections Test
-        Integer baseUser = 0;
-        Integer firstDegreeConnection = 749;
-        printSecondDegreeMutualFriends(baseUser, firstDegreeConnection);
+    //@Test
+    public void secondDegreeMutualConnectionsTest (){
+        testCG = new CapstoneDoSGraph();
+        loadGraph(testCG, filename);
+        Integer baseUser = 7;
+        //Integer firstDegreeConnection = 483;
+        //printSecondDegreeMutualFriends(baseUser, firstDegreeConnection);
+        printSecondDegreeMutualFriends(baseUser);
         System.out.println("----------------");
+    }
 
-        //end
-        System.out.println("================");
+    @Test
+    public void getAverageConnectionsAcrossDegreesOfSeparation (){
+        testCG = new CapstoneDoSGraph();
+        loadGraph(testCG, filename);
+        for (int i=1; i<=9; i++){
+            int avgAtDegree = getAverageConnectionsAtNDegrees(testCG,i);
+            System.out.println("Average of "+ avgAtDegree+" connections at "+i+" degrees of separation");
+            if (avgAtDegree == 0){
+                break;
+            }
+        }
+    }
+
+    private int getAverageConnectionsAtNDegrees (CapstoneDoSGraph g, Integer degree){
+        int total = 0;
+        for (int i=1; i < getNumNodes(); i++){
+            HashSet<Integer> connections = testCG.getNDegreeConnections(i, degree);
+            total += connections.size();
+        }
+        return total/getNumNodes();
     }
 
     /**
@@ -186,21 +225,39 @@ public class CapstoneTester {
         HashSet<Integer> connections = testCG.getNDegreeConnections(user, degree);
         System.out.println("There are "+connections.size()+" "+degree+" degree connections for user " + user);
         System.out.print("{");
-        //TODO add print for 2nd degree connections
         for(Integer connection : connections){
             System.out.print(connection+", ");
         }
         System.out.println(" }");
     }
 
-    public void printSecondDegreeMutualFriends(Integer baseUser, Integer firstDegreeConnection) {
-        HashMap<Integer,HashSet<Integer>> commonConnections = testCG.getSecondDegreeMutualFriends(baseUser, firstDegreeConnection);
-        for (Integer i : commonConnections.keySet()){
-            System.out.print(i+": ");
-            System.out.println(commonConnections.get(i));
+    public void printSecondDegreeMutualFriends(Integer baseUser) {
+        HashSet<Integer> commonConnections = testCG.getSecondDegreeMutualFriends(baseUser);
+        //print user friends
+        System.out.print("Primary ");
+        printConnections(baseUser);
+        //print firstDegreeConnection friends
+        System.out.println("Second Degree Connections: ");
+        printNthDegreeConnections(baseUser, 2);
+        //print potential shared connections
+        System.out.println("Third Degree Connections: ");
+        for (Integer connection : testCG.getNDegreeConnections(baseUser,2)){
+            printConnections(connection);
         }
+        //print shared connections
+        System.out.println("Mutual Connections with 2nd degree associations:");
+        System.out.print("{ ");
+        for (Integer i : commonConnections){
+            System.out.print(i+" ");
+        }
+        System.out.println("} ");
     }
 
+    public List<Integer> sortHashSet (HashSet<Integer> set){
+        List<Integer> sortedList = new ArrayList<Integer>(set);
+        Collections.sort(sortedList);
+        return sortedList;
+    }
 
     /**
      * method to loadGraph from simple txt file
